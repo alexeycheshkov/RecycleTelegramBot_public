37f8e9947c8e262ccb76a33b1b91ee9120dc27e3
AlexeyCheshkov <alexeychehskov@gmail.com>
2022-07-04 12:42:31 +0400

    v1.1

--- src/main/java/bot/MyRecycleBot.java
+++ src/main/java/bot/MyRecycleBot.java
@@ -11,9 +11,13 @@ import utils.FileUtils;
 import java.io.File;
 
 public class MyRecycleBot extends TelegramLongPollingBot {
-    private String time = "неизвестно, свяжитесь с нами удобным вам способом.";
-    private String botUserName = System.getenv("BOT_NAME");
-    private String botToken = System.getenv("BOT_TOKEN");
+    private static final String START = "/start";
+    private static final String SET_TIME = "/setTime";
+    private static final String PHOTO_SOURCE_PATH = "src/main/resources/photo/";
+    private static final String TEXT_SOURCE_PATH = "src/main/resources/description/";
+    private static final String BOT_USER_NAME = System.getenv("BOT_NAME");
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+    private static final String WORK_TIME_FILE_DIR = "src/main/resources/worktime.txt";
 
     public MyRecycleBot(DefaultBotOptions options) {
         super(options);
@@ -21,17 +25,17 @@ public class MyRecycleBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return botUserName;
+        return BOT_USER_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return botToken;
+        return BOT_TOKEN;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasCallbackQuery()){
+        if (update.hasCallbackQuery()) {
             recycleItemResponse(update.getCallbackQuery().getMessage().getChatId().toString(), update.getCallbackQuery().getData());
         }
         if (update.hasMessage() | update.hasCallbackQuery()) {
@@ -51,27 +55,8 @@ public class MyRecycleBot extends TelegramLongPollingBot {
 
     public synchronized void recycleItemResponse(String chatId, String itemType) {
         try {
-            switch (itemType) {
-                case "plastic":
-                    execute(SendPhoto.builder().chatId(chatId).photo(new InputFile(new File("src/main/resources/photo/plastic.png"))).build());
-                    execute(SendMessage.builder().chatId(chatId).text(FileUtils.readFile("src/main/resources/description/recycle/plastic.txt")).build());
-                    break;
-                case "fe_al":
-                    execute(SendPhoto.builder().chatId(chatId).photo(new InputFile(new File("src/main/resources/photo/fe_al.png"))).build());
-                    execute(SendMessage.builder().chatId(chatId).text(FileUtils.readFile("src/main/resources/description/recycle/fe_al.txt")).build());
-                    break;
-                case "glass":
-                    execute(SendPhoto.builder().chatId(chatId).photo(new InputFile(new File("src/main/resources/photo/glass.png"))).build());
-                    execute(SendMessage.builder().chatId(chatId).text(FileUtils.readFile("src/main/resources/description/recycle/glass.txt")).build());
-                    break;
-                case "paper":
-                    execute(SendPhoto.builder().chatId(chatId).photo(new InputFile(new File("src/main/resources/photo/paper.png"))).build());
-                    execute(SendMessage.builder().chatId(chatId).text(FileUtils.readFile("src/main/resources/description/recycle/paper.txt")).build());
-                    break;
-                default:
-                    execute(SendMessage.builder().text("Error.. Please try again.").build());
-                    break;
-            }
+            execute(SendPhoto.builder().chatId(chatId).photo(getPhotoByItem(itemType)).build());
+            execute(SendMessage.builder().chatId(chatId).text(getTextByItem(itemType)).build());
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -80,33 +65,39 @@ public class MyRecycleBot extends TelegramLongPollingBot {
     public synchronized void responseMsg(SendMessage message) {
         Keyboard.setMainMenuButtons(message);
         String chatId = message.getChatId();
-        if (message.getText().startsWith("/setTime")) {
-            time = message.getText().replace("/setTime ","");
-            FileUtils.writeWorkTime("src/main/resources/worktime.txt",time);
+        if (message.getText().startsWith(SET_TIME)) {
+            String time = message.getText().replace(SET_TIME, "").trim();
+            FileUtils.writeWorkTime(WORK_TIME_FILE_DIR, time);
         } else {
             try {
                 switch (message.getText()) {
-                    case "/start":
-                        message.setText("Приветствую тебя, эко-активист! Я могу рассказать о нашем движении, о сырьё которое мы собираем на переработку и о нашем пункте сбора.");
+                    case START:
+                        message.setText("Приветствую тебя, эко-активист!\nЯ могу рассказать о нашем движении, о сырье которое " +
+                                "мы собираем на переработку и о нашем пункте сбора.");
                         execute(message);
                         break;
                     case "Контакты":
                         message.setText("Остались вопросы? Звони:");
                         execute(message);
                         execute(SendContact.builder().chatId(chatId).firstName("Александра").phoneNumber("+79198443019").build());
-                        execute(SendContact.builder().chatId(chatId).firstName("Виктория").phoneNumber("+79510342670").build());
+                        execute(SendContact.builder().chatId(chatId).firstName("Надежда").phoneNumber("+79225580545").build());
                         Keyboard.setLinkButtons(message);
                         message.setText("Мы в соц сетях:");
                         execute(message);
                         break;
                     case "О нас":
-                        message.setText(FileUtils.readFile("src/main/resources/description/about.txt"));
+                        message.setText(getTextByItem("about"));
                         Keyboard.setAboutLinkButtons(message);
                         execute(message);
                         break;
                     case "Адреса":
-                        execute(SendVenue.builder().chatId(chatId).title("Пункт сбора сырья").address("г. Орск, ул. Короленко 132").longitude(58.456102).latitude(51.254518).build());
-                        message.setText("Ближайшее время сбора: " + FileUtils.readFile("src/main/resources/worktime.txt"));
+                        execute(SendVenue.builder().chatId(chatId).title("Пункт сбора сырья").address("г. Орск, ул. Короленко 132")
+                                .longitude(58.456102).latitude(51.254518).build());
+//                        String meetTime = FileUtils.readFile(WORK_TIME_FILE_DIR);
+//                        if (meetTime.isEmpty()) {
+//                            meetTime = "неизвестно, свяжитесь с нами удобным для вас способом.";
+//                        }
+//                        message.setText("Ближайшее время сбора: " + meetTime);
                         execute(message);
                         break;
                     case "Что принимаем":
@@ -123,5 +114,13 @@ public class MyRecycleBot extends TelegramLongPollingBot {
                 e.printStackTrace();
             }
         }
+    }
+
+    private InputFile getPhotoByItem(String item) {
+        return new InputFile(new File(PHOTO_SOURCE_PATH + item + ".png"));
+    }
+
+    private String getTextByItem(String item) {
+        return FileUtils.readFile(TEXT_SOURCE_PATH + item + ".txt");
     }
 }
